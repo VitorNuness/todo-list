1f63df7f55a8ce0d8b75eef4b77993cc8e6d6ec1
VitorNuness <vitor1nuness@gmail.com>
2024-10-22 14:25:20 -0300

    creating task service and adding list method to get all tasks sorted

---
+++ src/main/java/com/vitornunesdasilva/todo_list/service/TaskService.java
@@ -0,0 +1,27 @@
+package com.vitornunesdasilva.todo_list.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.vitornunesdasilva.todo_list.entity.Task;
+import com.vitornunesdasilva.todo_list.repository.TaskRepository;
+
+@Service
+public class TaskService {
+    private TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    List<Task> list() {
+        Sort sort = Sort.by(Direction.DESC, "isComplete")
+            .and(Sort.by(Direction.ASC, "date"))
+                .and(Sort.by(Direction.DESC, "priority")
+                        .and(Sort.by(Direction.ASC, "id")));
+        return taskRepository.findAll(sort);
+    }
+}
