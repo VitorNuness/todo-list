5b967c77dcdf92dd03df22f3713ae0e799892757
VitorNuness <vitor1nuness@gmail.com>
2024-10-22 14:08:32 -0300

    creating task entity

---
+++ src/main/java/com/vitornunesdasilva/todo_list/entity/Task.java
@@ -0,0 +1,72 @@
+package com.vitornunesdasilva.todo_list.entity;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private boolean isComplete;
+    private int priority;
+    private Date date;
+
+    public Task() {
+    }
+
+    public Task(Long id, String description, boolean isComplete, int priority, Date date) {
+        this.id = id;
+        this.description = description;
+        this.isComplete = isComplete;
+        this.priority = priority;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    public void setComplete(boolean isComplete) {
+        this.isComplete = isComplete;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
