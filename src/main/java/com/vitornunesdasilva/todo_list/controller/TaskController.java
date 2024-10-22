96b097c8f846e9fefa0c30e680df05b0555a17be
VitorNuness <vitor1nuness@gmail.com>
2024-10-22 14:40:45 -0300

    creating task controller and adding create method

---
+++ src/main/java/com/vitornunesdasilva/todo_list/controller/TaskController.java
@@ -0,0 +1,25 @@
+package com.vitornunesdasilva.todo_list.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vitornunesdasilva.todo_list.entity.Task;
+import com.vitornunesdasilva.todo_list.service.TaskService;
+
+@RestController
+@RequestMapping("tasks")
+public class TaskController {
+    @Autowired
+    private TaskService taskService;
+
+    @PostMapping
+    List<Task> create(@RequestBody Task task) {
+        return taskService.create(task);
+    }
+}
