73d4c114c0ec7acde2d32ce49a92387cca6e46a5
VitorNuness <vitor1nuness@gmail.com>
2024-10-23 18:23:27 -0300

    creating success and failure test for task create

--- src/test/java/com/vitornunesdasilva/todo_list/TodoListApplicationTests.java
+++ src/test/java/com/vitornunesdasilva/todo_list/TodoListApplicationTests.java
@@ -1,13 +1,50 @@
 package com.vitornunesdasilva.todo_list;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.time.LocalDate;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.vitornunesdasilva.todo_list.entity.Task;
+
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 class TodoListApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	void testCreateTaskSuccess() {
+		var task = new Task("task 1", "description task 1", false, 1, LocalDate.now());
+
+		webTestClient
+			.post()
+            .uri("/tasks")
+            .bodyValue(task)
+            .exchange()
+            .expectStatus().isOk()
+            .expectBody()
+            .jsonPath("$").isArray()
+            .jsonPath("$.length()").isEqualTo(1)
+            .jsonPath("$[0].name").isEqualTo(task.getName())
+            .jsonPath("$[0].description").isEqualTo(task.getDescription())
+            .jsonPath("$[0].complete").isEqualTo(task.isComplete())
+            .jsonPath("$[0].priority").isEqualTo(task.getPriority())
+            .jsonPath("$[0].date").isEqualTo(task.getDate().toString());
 	}
 
+    @Test
+	void testCreateTaskFailure() {
+		var task = new Task("", "", false, 0, LocalDate.now());
+
+		webTestClient
+			.post()
+            .uri("/tasks")
+            .bodyValue(task)
+            .exchange()
+            .expectStatus().isBadRequest();
+	}
 }
