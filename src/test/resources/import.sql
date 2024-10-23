21f02b969523f4b890de6305206f7e885553bbb2
VitorNuness <vitor1nuness@gmail.com>
2024-10-23 19:42:48 -0300

    creating files to seed tasks in database for tests

---
+++ src/test/resources/import.sql
@@ -0,0 +1,12 @@
+DELETE FROM tasks;
+
+INSERT INTO tasks (id, name, description, is_complete, priority, date)
+ VALUES (9995, 'task', 'description', false, 1, CURRENT_DATE);
+INSERT INTO tasks (id, name, description, is_complete, priority, date)
+ VALUES (9996, 'task', 'description', false, 1, CURRENT_DATE);
+INSERT INTO tasks (id, name, description, is_complete, priority, date)
+ VALUES (9997, 'task', 'description', false, 1, CURRENT_DATE);
+INSERT INTO tasks (id, name, description, is_complete, priority, date)
+ VALUES (9998, 'task', 'description', false, 1, CURRENT_DATE);
+INSERT INTO tasks (id, name, description, is_complete, priority, date)
+ VALUES (9999, 'task', 'description', false, 1, CURRENT_DATE);
